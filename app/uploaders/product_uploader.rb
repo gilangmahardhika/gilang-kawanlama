7befd8280d39799c059ed4d2a957e6ef559105de
gilang mahardhika <gilangmahardhika@gmail.com>
2024-01-13 18:15:49 +0700

    add cloudinary for production

--- app/uploaders/product_uploader.rb
+++ app/uploaders/product_uploader.rb
@@ -2,15 +2,22 @@ class ProductUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  if Rails.env.production?
+    include Cloudinary::CarrierWave
+  end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  unless Rails.env.production?
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  unless Rails.env.production?
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
