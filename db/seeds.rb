f9f2613fa9f984c2e49595bcf209c5ee450bae69
Gilang Mahardhika <gilangmahardhika@gmail..com>
2024-01-30 02:40:58 +0700

    change seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,19 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-(1..55).each do
-  Product.create do |p|
-    p.name = Faker::Commerce.product_name
-    p.sku = Faker::Barcode.ean(13)
-    p.count =  rand(1..100)
-    p.remote_first_photo_url = Faker::LoremFlickr.image(size: "150x150")
+(1..500000).each.with_index(1) do |i|
+  begin
+    Product.create! do |p|
+      p.name = Faker::Commerce.product_name
+      p.sku = Faker::Barcode.ean(13)
+      p.count =  rand(1..100)
+      p.description = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true)
+      # p.remote_first_photo_url = Faker::LoremFlickr.image(size: "150x150")
+    end
+    puts "create #{i}"
+  rescue
+    puts "failed #{i}"
   end
-  puts "*"
 end
 
 # Seeds Admin User
